a6c4e44d8236d226c8a71b4af4fed0de3e6c145a
Alexander Chisholm <alex@alexanders-mbp.attlocal.net>
2020-09-27 16:07:44 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app! You know, if you want to...
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
